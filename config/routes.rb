a127c3f9aa13453f525567fa1054c74907eb9922
github username <github email address>
2019-12-16 11:32:51 +0000

    spelling error fixed

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,10 @@ Rails.application.routes.draw do
       resources :users, only: [:create]
       post '/login', to: 'users#login'
       get '/validate', to: 'users#validate'
+      resources :user_recipes
+      post '/user_recipes', to: 'user_recipes#create'
     end
   end
+  # resources :UserRecipe, only: [:create]
+  # post 'create', to: 'user_resipes#create'
 end
---
+++ test/controllers/api/v1/user_recipe_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class Api::V1::UserRecipeControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
