917854e43c2d23c8d506672e31491be13997861c
github username <github email address>
2019-12-29 14:39:10 +0000

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :users
+      root 'users#login'
       post '/login', to: 'users#login'
       get '/validate', to: 'users#validate'
       resources :user_recipes, only: [:create, :index]
